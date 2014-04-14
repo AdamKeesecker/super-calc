482b70720dd6dbdf3e30b28ba1fa0d1f7c662073
Adam Keesecker <adamkeesecker@gmail.com>
2014-04-14 12:03:08 -0500

    mostly done. fixes req: factorial clear buttons

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -9,8 +9,57 @@
     $('.decimal').click(dec);
     $('.sign').click(sign);
     $('#title').click(title);
+    $('.push').click(push);
+    $('.operator').click(operator);
   }
+  function operator(){
+    var op=$(this).data('op');
+    var x = $('#queue > div:nth-child(1)').text()*1;
+    var y = $('#queue > div:nth-child(2)').text()*1;
+    var value;
+    switch(op){
+      case 'add':
+        value=x+y;
+        break;
+      case 'sub':
+        value=x-y;
+        break;
+      case 'mul':
+        value=x*y;
+        break;
+      case 'div':
+        value=x/y;
+        break;
+      case 'exp':
+        value= Math.pow(x,y);
+        break;
+      case 'root':
+        value= Math.sqrt(x,y);
+        break;
+      case 'fact':
+        value=fact(x);
+        break;
+      case 'sum':
+        value=sum();
+        }
+    $('#display').text(value);
+    }
+  function sum(){
+    var total = 0;
+    $('#queue > div').each(function(index, div){
+      var x = div.textContent*1;
+      total += x;
+    });
+    return total;
 
+  }
+  function fact(x){
+    var product = 1;
+    for(var i = 2; i <= value; i++){
+      product *= i;
+    }
+    return product;
+  }
   function title(){
     var display = $('#calculator').css('display');
     if(display==='none'){
@@ -19,13 +68,17 @@
     else{
       $('#calculator').fadeOut();
     }
-
   }
 
   function display(){
     var num = this.textContent;
     var out = $('#display').text();
-    out += num;
+    if(out==='0'){
+      out = num;
+    }
+    else{
+      out += num;
+    }
     var x = out.indexOf('.');
     if (x === -1){
       $('#display').text(out);
@@ -33,13 +86,16 @@
     else{
       $('#display').text(out);
     }
-
   }
 
   function clear(){
     var symbol = this.textContent;
-    if(symbol === 'C'){
+    if(symbol === 'clear'){
       $('#display').text('0');
+    }
+    else if(symbol==='CB'){
+      $('#queue').empty();
+      $('#display').text('0')
     }
   }
   function dec(){
@@ -54,6 +110,13 @@
   function sign(){
     var display = $('#display').text();
     $('#display').text(display * -1);
+  }
+  function push(){
+    var display = $('#display').text();
+    $('#display').text(0); //clear display to 0//
+    var $div = $('<div>');
+    $div.text(display);
+    $('#queue').prepend($div);
   }
 
 
