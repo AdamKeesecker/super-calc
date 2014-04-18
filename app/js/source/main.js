93aa18d1d53e5a10d6a5dedafcb818b023841663
Adam Keesecker <adamkeesecker@gmail.com>
2014-04-17 20:10:08 -0500

    final

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -1,125 +1,126 @@
 (function(){
   'use strict';
 
-  $('document').ready(initialize);
+  $(document).ready(initialize);
 
   function initialize(){
-    $('.number').click(display);
-    $('.clear').click(clear);
-    $('.decimal').click(dec);
-    $('.sign').click(sign);
     $('#title').click(title);
+    $('.number').click(number);
+    $('.clear').click(clear);
+    $('.decimal').click(decimal);
+    $('.sign').click(sign);
     $('.push').click(push);
     $('.operator').click(operator);
   }
+
   function operator(){
-    var op=$(this).data('op');
-    var x = $('#queue > div:nth-child(1)').text()*1;
-    var y = $('#queue > div:nth-child(2)').text()*1;
-    var value;
+    var op = $(this).data('op');
+    var x = $('#queue > div:nth-child(1)').text() * 1;
+    var y = $('#queue > div:nth-child(2)').text() * 1;
+    var result;
+
     switch(op){
       case 'add':
-        value=x+y;
+        result = x + y;
         break;
       case 'sub':
-        value=x-y;
+        result = x - y;
         break;
       case 'mul':
-        value=x*y;
+        result = x * y;
         break;
       case 'div':
-        value=x/y;
+        result = x / y;
         break;
       case 'exp':
-        value= Math.pow(x,y);
+        result = Math.pow(x, y);
         break;
       case 'root':
-        value= Math.sqrt(x,y);
+        result = Math.sqrt(x);
         break;
       case 'fact':
-        value=fact(x);
+        result = factorial(x);
         break;
       case 'sum':
-        value=sum();
-        }
-    $('#display').text(value);
+        result = sum();
     }
+
+    $('#display').text(result);
+  }
+
   function sum(){
     var total = 0;
+
     $('#queue > div').each(function(index, div){
-      var x = div.textContent*1;
+      var x = div.textContent * 1;
       total += x;
     });
-    return total;
 
+    return total;
   }
-  function fact(x){
+
+  function factorial(x){
     var product = 1;
-    for(var i = 2; i <= value; i++){
+
+    for(var i = 2; i <= x; i++){
       product *= i;
     }
+
     return product;
   }
-  function title(){
-    var display = $('#calculator').css('display');
-    if(display==='none'){
-      $('#calculator').fadeIn();
-    }
-    else{
-      $('#calculator').fadeOut();
-    }
-  }
 
-  function display(){
-    var num = this.textContent;
-    var out = $('#display').text();
-    if(out==='0'){
-      out = num;
-    }
-    else{
-      out += num;
-    }
-    var x = out.indexOf('.');
-    if (x === -1){
-      $('#display').text(out);
-    }
-    else{
-      $('#display').text(out);
-    }
-  }
-
-  function clear(){
-    var symbol = this.textContent;
-    if(symbol === 'clear'){
-      $('#display').text('0');
-    }
-    else if(symbol==='CB'){
-      $('#queue').empty();
-      $('#display').text('0')
-    }
-  }
-  function dec(){
-    var point = $('#display').text();
-    if(point.indexOf('.')=== -1){
-      $('#display').text(point +'.');
-    }
-    else{
-      $('#display').text();
-    }
-  }
-  function sign(){
-    var display = $('#display').text();
-    $('#display').text(display * -1);
-  }
   function push(){
     var display = $('#display').text();
-    $('#display').text(0); //clear display to 0//
+    $('#display').text(0);
     var $div = $('<div>');
     $div.text(display);
     $('#queue').prepend($div);
   }
 
+  function title(){
+    var display = $('#calculator').css('display');
 
+    if(display === 'none'){
+      $('#calculator').fadeIn();
+    }else{
+      $('#calculator').fadeOut();
+    }
+  }
 
+  function number(){
+    var num = this.textContent;
+    var output = $('#display').text();
 
+    if(output === '0'){
+      output = num;
+    }else{
+      output += num;
+    }
+
+    $('#display').text(output);
+  }
+
+  function clear(){
+    var type = this.textContent;
+
+    if(type === 'C'){
+      $('#display').text(0);
+    }else{
+      $('#queue').empty();
+    }
+  }
+
+  function decimal(){
+    var display = $('#display').text();
+    var noDecimal = display.indexOf('.') === -1;
+
+    if(noDecimal){
+      $('#display').text(display + '.');
+    }
+  }
+
+  function sign(){
+    var display = $('#display').text();
+    $('#display').text(display * -1);
+  }
 })();
